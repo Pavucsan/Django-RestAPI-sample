6a46c4c7793500e377c98e72cb827a360e1289a8
Pavucs Manokar <37257839+Pavucsan@users.noreply.github.com>
2019-12-22 02:10:04 +0530

    user and group api created

--- sample_api/account/views.py
+++ sample_api/account/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from account.serializers.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+from rest_framework import viewsets
+
+class UserViewSet(viewsets.ModelViewSet):
+    # API endpoint that allows to be viewed or edited
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
